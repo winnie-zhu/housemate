eba9927fbcf5d1b5171e9c81ecc69461f5b41714
Winnie Zhu <winniezhu@berkeley.edu>
2022-12-01 20:05:55 -0800

    test

---
+++ HouseMate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HouseMate
+//
+//  Created by Winnie Zhu on 12/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HouseMate/HouseMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HouseMateApp.swift
+//  HouseMate
+//
+//  Created by Winnie Zhu on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct HouseMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
